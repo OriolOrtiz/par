8972640239e6a44eddb1918bbcadec6902466d62
Héctor Ramón Jiménez <hector0193@gmail.com>
2014-04-02 23:01:31 +0200

    RubyTeX has born! :D

---
+++ lab0/second/rubytex.rb
@@ -0,0 +1,70 @@
+def method_missing(m, *args, options: nil, special: false, &block)
+  m = m.to_s
+
+  return if m == 'to_hash'
+
+  if block_given?
+    print "\\begin{#{m}}"
+    print_options(options)
+    print_args(args)
+
+    puts
+
+    yield
+
+    puts "\\end{#{m}}"
+  else
+    print "\\#{m}"
+    print '*' if special
+    print_options(options)
+    print_args(args)
+
+    puts
+  end
+end
+
+def print_options(options)
+    print "[#{options.to_a.join(",")}]" unless options.nil?
+end
+
+def print_args(args)
+    args.each {|arg| print "{#{arg}}"} if args
+end
+
+def escape(s)
+  s.gsub('_', '\\_')
+end
+
+def wrap()
+    print "{"
+    yield
+    puts "}"
+end
+
+def br(size=nil, units: "cm")
+    print "\\\\"
+    print "[#{size}#{units}]" unless size.nil?
+    puts
+end
+
+def head(*args)
+  row *args.map{|arg| "\\textbf{#{arg}}"}
+end
+
+def row(*args)
+  hline
+  puts escape args.join(' & ')
+  br
+end
+
+class String
+  def to_a()
+    self.split(',')
+  end
+end
+
+class Array
+  def to_a()
+    self
+  end
+end
